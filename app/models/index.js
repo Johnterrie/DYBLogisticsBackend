cbd6439182618c0743d510d4fcd6593da13044d3
Osumah John Terry <osumahterry@gmail.com>
2025-07-08 23:37:17 +0100

    added: shippments models

--- app/models/index.js
+++ app/models/index.js
@@ -1,3 +1,8 @@
-import User from "./user.js";
+import User from "./user/index.js"
+import Contact from "./contact/index.js";
+import Shipping from "./shipping/index.js"
+import Tracking from "./tracking/index.js"
+import Dashboard from "./dashboard/index.js"
+import Rate from "./rate/index.js"
 
-export { User }
+export { User, Contact, Shipping, Tracking, Dashboard, Rate }
